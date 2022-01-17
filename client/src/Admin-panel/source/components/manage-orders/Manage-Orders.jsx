7fe009e4f8779a75319cd4560f6c85f86aff9ae3
Mohammad Idrees <idreeeeees1999@gmail.com>
2022-01-17 05:33:14 +0500

    1

--- client/src/Admin-panel/source/components/manage-orders/Manage-Orders.jsx
+++ client/src/Admin-panel/source/components/manage-orders/Manage-Orders.jsx
@@ -5,142 +5,201 @@ import axios from "../../axiosInstance";
 import axios1 from "axios";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 let ManageOrders = (props) => {
+	let [data, setData] = useState();
 
+	let getData = () => {
+		axios
+			.get("/order")
+			.then((res) => {
+				let arr = res.data;
 
-	let [data,setData] = useState();
+				setData(
+					arr.filter((x) => {
+						return x.status !== "delivered" && x.Shipping_Info;
+					})
+				);
+			})
+			.catch((err) => {
+				console.log("error");
+			});
+	};
 
-	let getData = ()=>{
-		axios.get('/order').then(res=>{
-			let arr = res.data;
-		
-			setData(	arr.filter(x=>{
-				return x.status !== "delivered" && x.Shipping_Info
-			}));
-		}).catch(err=>{
-			console.log('error')
-		})
-
-	}
-
-	useEffect(()=>{
-
+	useEffect(() => {
 		getData();
-	},[])
+	}, []);
 
+	let shipBtnHandler = async (orderId, products) => {
+		let array = [];
 
-	let shipBtnHandler = async (orderId,products)=>{
-let array= [];
-
-		for(let item of products){
-			if(item.category ==="accessories" || item.category ==="pcParts"){
-				console.log(item)
-				await axios.get('/'+item.category+"/"+item.category1+"/"+item.product_id,).then(res=>{
-					let i = res.data;
-					i.quantity = i.quantity - item.quantity;
-					array.push(i);
-				}).catch(err=>{
-					console.log('error');
-				})
-			}else{
-				await axios.get('/'+item.category+"/"+item.product_id,).then(res=>{
-					let i = res.data;
-					i.quantity = i.quantity - item.quantity;
-					array.push(i);
-				}).catch(err=>{
-					console.log('error');
-				})
+		for (let item of products) {
+			if (item.category === "accessories" || item.category === "pcParts") {
+				console.log(item);
+				await axios
+					.get(
+						"/" + item.category + "/" + item.category1 + "/" + item.product_id
+					)
+					.then((res) => {
+						let i = res.data;
+						i.quantity = i.quantity - item.quantity;
+						array.push(i);
+					})
+					.catch((err) => {
+						console.log("error");
+					});
+			} else {
+				await axios
+					.get("/" + item.category + "/" + item.product_id)
+					.then((res) => {
+						let i = res.data;
+						i.quantity = i.quantity - item.quantity;
+						array.push(i);
+					})
+					.catch((err) => {
+						console.log("error");
+					});
 			}
-			
 		}
 
 		console.log(array);
 
-		for(let item of array){
-			let obj= {
-				quantity: item.quantity
-			}
+		for (let item of array) {
+			let obj = {
+				quantity: item.quantity,
+			};
 
-
-			if(item.category ==="accessories" || item.category ==="pcParts"){
-				await axios1.put('http://localhost:4000/api/'+item.category+"/"+item.category1+"/quantity/"+item._id,obj).then(res=>{
-					console.log(res.data)
-					}).catch(err=>{
-						console.log('error');
-					})	
-			}
-			else{
-				await axios1.put('http://localhost:4000/api/'+item.category+"/quantity/"+item._id,obj).then(res=>{
-					console.log(res.data)
-					}).catch(err=>{
-						console.log('error');
+			if (item.category === "accessories" || item.category === "pcParts") {
+				await axios1
+					.put(
+						"http://localhost:4000/api/" +
+							item.category +
+							"/" +
+							item.category1 +
+							"/quantity/" +
+							item._id,
+						obj
+					)
+					.then((res) => {
+						console.log(res.data);
 					})
+					.catch((err) => {
+						console.log("error");
+					});
+			} else {
+				await axios1
+					.put(
+						"http://localhost:4000/api/" +
+							item.category +
+							"/quantity/" +
+							item._id,
+						obj
+					)
+					.then((res) => {
+						console.log(res.data);
+					})
+					.catch((err) => {
+						console.log("error");
+					});
 			}
-
-			
 		}
 
+		axios
+			.put("/order/shipped/" + orderId)
+			.then((res) => {
+				console.log("shipped");
+				getData();
+			})
+			.catch((err) => {
+				console.log("error");
+			});
+	};
 
-		axios.put('/order/shipped/'+orderId).then(res=>{
-			console.log("shipped");
-			getData();
-		}).catch(err=>{
-			console.log("error")
-		})
+	let deliveredBtnHandler = (id) => {
+		axios
+			.put("/order/delivered/" + id)
+			.then((res) => {
+				console.log("delivered");
+				getData();
+			})
+			.catch((err) => {
+				console.log("error");
+			});
+	};
+
+	let td = <LoadingSpinner />;
+
+	if (data) {
+		td = data.map((order) => {
+			return (
+				<tr key={order._id}>
+					<td>{order._id}</td>
+					<td>
+						{order.products.map((x) => {
+							return (
+								<ul>
+									<li>
+										<strong>Title :</strong>
+										{x.title}
+									</li>
+									<li>
+										<strong>Quantity:</strong> {x.quantity}
+									</li>
+								</ul>
+							);
+						})}
+					</td>
+					{order.Shipping_Info ? (
+						<td>
+							{
+								<ul>
+									<li>
+										<strong>Address:</strong> {order.Shipping_Info.address}
+									</li>
+									<li>
+										<strong>City:</strong> {order.Shipping_Info.city}
+									</li>
+									<li>
+										<strong>Street:</strong> {order.Shipping_Info.street}
+									</li>
+									<li>
+										<strong>State:</strong> {order.Shipping_Info.state}
+									</li>
+									<li>
+										<strong>Country:</strong> {order.Shipping_Info.country}
+									</li>
+								</ul>
+							}
+						</td>
+					) : (
+						<td>-------</td>
+					)}
+
+					<td>
+						<strong>{order.totalPrice} PKR</strong>
+					</td>
+					<td>Cash on Delivery</td>
+					<td>{order.user.userName}</td>
+					<td>{order.user.user_PhoneNumber}</td>
+					<td>
+						{order.status === "shipped" ? (
+							<button
+								onClick={() => deliveredBtnHandler(order._id)}
+								className={classes.deleteBtn1}
+							>
+								Delivered
+							</button>
+						) : (
+							<button
+								onClick={() => shipBtnHandler(order._id, order.products)}
+								className={classes.updateBtn1}
+							>
+								Ship
+							</button>
+						)}
+					</td>
+				</tr>
+			);
+		});
 	}
-
-	let deliveredBtnHandler = (id)=>{
-		axios.put('/order/delivered/'+id).then(res=>{
-			console.log("delivered")
-			getData();
-		}).catch(err=>{
-			console.log("error")
-		})
-	}
-
-	let td = <LoadingSpinner/>;
-
-	if(data){
-
-		td = data.map(order=>{
-			
-	return(	<tr key={order._id}>
-		<td>{data.indexOf(order)+1}</td>
-		<td>{order._id}</td>
-		<td>
-			{order.products.map(x=>{
-				return (<ul>
-				<li><strong>Title :</strong>{x.title}</li>
-				<li><strong>Quantity:</strong> {x.quantity}</li>
-			</ul>)
-			})}
-		</td>
-		{order.Shipping_Info ? <td>{
-			<ul>
-				<li><strong>Address:</strong>  {order.Shipping_Info.address}</li>
-				<li><strong>City:</strong> {order.Shipping_Info.city}</li>
-				<li><strong>Street:</strong> {order.Shipping_Info.street}</li>
-				<li><strong>State:</strong> {order.Shipping_Info.state}</li>
-				<li><strong>Country:</strong> {order.Shipping_Info.country}</li>
-			</ul>
-			}</td> : <td>-------</td>
-	}
-		
-		<td><strong>{order.totalPrice} PKR</strong></td>
-		<td>Cash on Delivery</td>
-		<td>{order.user.userName}</td>
-		<td>{order.user.user_PhoneNumber}</td>
-		<td>{order.status==="shipped"? 
-		<button onClick={()=>deliveredBtnHandler(order._id)} className={classes.deleteBtn1}>Delivered</button>
-		: <button  onClick={()=>shipBtnHandler(order._id,order.products)} className={classes.updateBtn1}>Ship</button>}</td>
-	</tr>)
-		})
-		
-		
-		
-	}
-
-
-
 
 	return (
 		<div>
@@ -155,17 +214,18 @@ let array= [];
 				</div>
 				<div className={classes.tab1}>
 					<table className={classes.container1}>
-						{data ? <tr>
-							<th className={classes.id1}>Sr#</th>
-							<th className={classes.id1}>Order ID</th>
-							<th className={classes.items1}>Items List</th>
-							<th className={classes.items1}>Shipping Info.</th>
-							<th className={classes.price11}>Total Price</th>
-							<th className={classes.payment1}>Payment Method</th>
-							<th className={classes.date1}>User Name</th>
-							<th className={classes.date1}>User Phone#</th>
-							<th className={classes.date1}>Actions</th>
-						</tr>: null}
+						{data ? (
+							<tr>
+								<th className={classes.id1}>Order ID</th>
+								<th className={classes.items1}>Items List</th>
+								<th className={classes.shippinginfo12}>Shipping Info.</th>
+								<th className={classes.price11}>Total Price</th>
+								<th className={classes.payment1}>Payment Method</th>
+								<th className={classes.date1}>User Name</th>
+								<th className={classes.date1}>User Phone#</th>
+								<th className={classes.date1}>Actions</th>
+							</tr>
+						) : null}
 						{td}
 					</table>
 				</div>
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,6 +6,18 @@ const Home = () => {
 	return (
 		<div>
 			<HomeCarousel />
+			<div className="container mt-5">
+				<div className="row">
+					<div className="col-lg-6"></div>
+					<div className="col-lg-6">
+						<iframe
+							width="100%"
+							height="400"
+							src="https://www.youtube.com/embed/MCW-kevlDjo?controls=1"
+						></iframe>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
